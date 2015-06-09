65559dd33962a41a70ea5eb94e99d030789ff706
shoguone <avlal.nsu@gmail.com>
2015-06-09 10:22:54 +0600

    Рабочий монитор, залит последний из проектов
    Требуется поправить сортировку при добавлении новой команды
    #дадаопять

---
+++ Monitor/MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using Monitor.EntityModels;
+using Monitor.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace Monitor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int minFont = Properties.Settings.Default.GridFontMin;
+        private int maxFont = Properties.Settings.Default.GridFontMax;
+        private int fontSize = Properties.Settings.Default.GridFont;
+
+        public bool IsFullScreen = false;
+
+        private Thread _updatingThread;
+
+        private ObservableCollection<MonitorCrew> crews = new ObservableCollection<MonitorCrew>();
+
+        private delegate void CrewsParameterDelegate(List<MonitorCrew> crews);
+        private delegate void KomParameterDelegate(List<kom> data);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Init();
+
+            this.Width = 1360;
+            this.Height = 768;
+
+            //this.Width = System.Windows.SystemParameters.VirtualScreenWidth - System.Windows.SystemParameters.FullPrimaryScreenWidth;
+            //this.Height = this.Width * 9 / 16;
+
+            //this.Height = System.Windows.SystemParameters.FullPrimaryScreenHeight;
+
+            //CrewDataGrid.ItemsSource = crews;
+            //Binding myBinding = new Binding();
+            //myBinding.Source = crews;
+            //CrewDataGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
+            //Monitor.Properties.
+
+            Init();
+            CrewDataGrid.ItemsSource = crews;
+
+            _updatingThread = new Thread(new ThreadStart(Update)) { IsBackground = true };
+            _updatingThread.Start();
+        }
+
+        private void Update()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                Init();
+
+            }
+        }
+
+        private void Init()
+        {
+            CurrentSituationTextBlock.Dispatcher.BeginInvoke(new Action(UpdateCurrentSituationTextBlock));
+
+            using (EntityContext ctx = new EntityContext())
+            {
+                var data = ctx.kom
+                    .Where(k => k.FL_DISPLAY == 1 && k.FL_UB != 1)
+                    .OrderBy(k => k.D_OTPR)
+                    .ThenBy(k => k.N_KOM)
+                    .ToList();
+
+                //crews.Clear();
+
+                CrewDataGrid.Dispatcher.BeginInvoke(new KomParameterDelegate(UpdateGrid), data);
+                
+                //CrewDataGrid.ItemsSource = data;
+                //CrewDataGrid.ItemsSource = crews;
+
+                //Binding myBinding = new Binding();
+                //myBinding.Source = crews;
+
+                //CrewDataGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
+                //CrewDataGrid.Dispatcher.BeginInvoke(new CrewsParameterDelegate(BindCrews), crews);
+                //BindingExpression seasonsBindingExpression = CrewDataGrid.GetBindingExpression(ListView.ItemsSourceProperty);
+
+                //CrewDataGrid.Dispatcher.BeginInvoke(new Action(UpdateGrid));
+            }
+        }
+
+        //private void BindCrews(List<MonitorCrew> cs)
+        //{
+        //    Binding myBinding = new Binding();
+        //    myBinding.Source = crews;
+
+        //    CrewDataGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
+        //}
+
+        private void UpdateCurrentSituationTextBlock()
+        {
+            CurrentSituationTextBlock.Text = DateTime.Now.ToString("dd MMMM yyyy г, dddd, HH:mm");
+        }
+
+        private void UpdateGrid(List<kom> data)
+        {
+            for (int i = 0; i < crews.Count; i++)
+            {
+                if (!data.Any(d => d.N_KOM == crews[i].CrewNumber))
+                {
+                    crews.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    var kom = data.First(d => d.N_KOM == crews[i].CrewNumber);
+                    crews[i].UpdateWithKom(kom);
+                    data.Remove(kom);
+                }
+            }
+            if (data.Count > 0)
+            {
+                foreach (var kom in data)
+                {
+                    crews.Add(new MonitorCrew(kom));
+                }
+                crews
+                    .OrderBy(c => c.DepartureDate)
+                    .ThenBy(c => c.CrewNumber);
+
+            }
+        }
+
+        /*private void UpdateGrid()
+        {
+            CrewDataGrid.ItemsSource = null;
+
+            //Binding myBinding = new Binding();
+            //myBinding.Source = crews;
+
+            //CrewDataGrid.SetBinding(ListView.ItemsSourceProperty, myBinding);
+
+            Binding myBinding = new Binding() { Source = crews };
+            CrewDataGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
+            
+            BindingExpression seasonsBindingExpression = CrewDataGrid.GetBindingExpression(ListView.ItemsSourceProperty);
+            seasonsBindingExpression.UpdateTarget();
+        }*/
+
+        private void CrewDataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            var displayName = GetPropertyDisplayName(e.PropertyDescriptor);
+
+            if (!string.IsNullOrEmpty(displayName))
+            {
+                e.Column.Header = displayName;
+            }
+
+        }
+
+        public static string GetPropertyDisplayName(object descriptor)
+        {
+            var pd = descriptor as PropertyDescriptor;
+
+            if (pd != null)
+            {
+                // Check for DisplayName attribute and set the column header accordingly
+                var displayName = pd.Attributes[typeof(DisplayNameAttribute)] as DisplayNameAttribute;
+
+                if (displayName != null && displayName != DisplayNameAttribute.Default)
+                {
+                    return displayName.DisplayName;
+                }
+
+            }
+            else
+            {
+                var pi = descriptor as PropertyInfo;
+
+                if (pi != null)
+                {
+                    // Check for DisplayName attribute and set the column header accordingly
+                    Object[] attributes = pi.GetCustomAttributes(typeof(DisplayNameAttribute), true);
+                    for (int i = 0; i < attributes.Length; ++i)
+                    {
+                        var displayName = attributes[i] as DisplayNameAttribute;
+                        if (displayName != null && displayName != DisplayNameAttribute.Default)
+                        {
+                            return displayName.DisplayName;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void EnterFullScreenMode()
+        {
+            this.WindowStyle = System.Windows.WindowStyle.None;
+            this.WindowState = System.Windows.WindowState.Maximized;
+
+            IsFullScreen = true;
+        }
+
+        public void ExitFullScreenMode()
+        {
+            this.WindowStyle = System.Windows.WindowStyle.SingleBorderWindow;
+            this.WindowState = System.Windows.WindowState.Normal;
+
+            IsFullScreen = false;
+        }
+
+        private void FullScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsFullScreen)
+                ExitFullScreenMode();
+            else
+                EnterFullScreenMode();
+        }
+
+        private void CrewDataGrid_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if ((Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)) &&
+                e.Delta != 0)
+            {
+                double f = CrewDataGrid.FontSize + (double)e.Delta / 240;
+                if (f > maxFont)
+                    f = maxFont;
+                if (f < minFont)
+                    f = minFont;
+                CrewDataGrid.FontSize = f;
+
+                e.Handled = true;
+            }
+        }
+
+    }
+}
---
+++ Monitor/Utility/MonitorCrew.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Monitor.EntityModels;
+using System.ComponentModel;
+
+namespace Monitor.Utility
+{
+    public class MonitorCrew : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public static string ElderArrived = "прибыл";
+
+        private string _departureDate;
+        [DisplayName("Дата отправки")]
+        public string DepartureDate
+        {
+            get { return _departureDate; }
+            set
+            {
+                if (_departureDate != value)
+                {
+                    _departureDate = value;
+                    _OnPropertyChanged(this, "DepartureDate");
+                }
+            }
+        }
+
+        private string _crewNumber;
+        [DisplayName("№ ОСП")]
+        public string CrewNumber
+        {
+            get { return _crewNumber; }
+            set
+            {
+                if (_crewNumber != value)
+                {
+                    _crewNumber = value;
+                    _OnPropertyChanged(this, "CrewNumber");
+                }
+            }
+        }
+
+        private string _forcesType;
+        [DisplayName("Вид войск")]
+        public string ForcesType
+        {
+            get { return _forcesType; }
+            set
+            {
+                if (_forcesType != value)
+                {
+                    _forcesType = value;
+                    _OnPropertyChanged(this, "ForcesType");
+                }
+            }
+        }
+
+        private string _district;
+        [DisplayName("ВО")]
+        public string District
+        {
+            get { return _district; }
+            set
+            {
+                if (_district != value)
+                {
+                    _district = value;
+                    _OnPropertyChanged(this, "District");
+                }
+            }
+        }
+
+        private string _rrStation;
+        [DisplayName("Станция")]
+        public string RRStation
+        {
+            get { return _rrStation; }
+            set
+            {
+                if (_rrStation != value)
+                {
+                    _rrStation = value;
+                    _OnPropertyChanged(this, "RRStation");
+                }
+            }
+        }
+
+        private string _unit;
+        [DisplayName("В/ч")]
+        public string Unit
+        {
+            get { return _unit; }
+            set
+            {
+                if (_unit != value)
+                {
+                    _unit = value;
+                    _OnPropertyChanged(this, "Unit");
+                }
+            }
+        }
+
+        private string _duty;
+        [DisplayName("Наряд ГШ")]
+        public string Duty
+        {
+            get { return _duty; }
+            set
+            {
+                if (_duty != value)
+                {
+                    _duty = value;
+                    _OnPropertyChanged(this, "Duty");
+                }
+            }
+        }
+
+        private string _overall;
+        [DisplayName("Всего")]
+        public string Overall
+        {
+            get { return _overall; }
+            set
+            {
+                if (_overall != value)
+                {
+                    _overall = value;
+                    _OnPropertyChanged(this, "Overall");
+                }
+            }
+        }
+
+        private string _va;
+        [DisplayName("ВА")]
+        public string VA
+        {
+            get { return _va; }
+            set
+            {
+                if (_va != value)
+                {
+                    _va = value;
+                    _OnPropertyChanged(this, "VA");
+                }
+            }
+        }
+
+        private string _mtlb;
+        [DisplayName("МТЛБ")]
+        public string MTLB
+        {
+            get { return _mtlb; }
+            set
+            {
+                if (_mtlb != value)
+                {
+                    _mtlb = value;
+                    _OnPropertyChanged(this, "MTLB");
+                }
+            }
+        }
+
+        private string _access;
+        [DisplayName("Есть допуcк")]
+        public string Access
+        {
+            get { return _access; }
+            set
+            {
+                if (_access != value)
+                {
+                    _access = value;
+                    _OnPropertyChanged(this, "Access");
+                }
+            }
+        }
+
+        private string _elder;
+        [DisplayName("Старший")]
+        public string Elder
+        {
+            get { return _elder; }
+            set
+            {
+                if (_elder != value)
+                {
+                    _elder = value;
+                    _OnPropertyChanged(this, "Elder");
+                }
+            }
+        }
+
+        private bool _dressUp;
+        [DisplayName("Переодевание")]
+        public bool DressUp
+        {
+            get { return _dressUp; }
+            set
+            {
+                if (_dressUp != value)
+                {
+                    _dressUp = value;
+                    _OnPropertyChanged(this, "DressUp");
+                }
+            }
+        }
+
+        private bool _isSent;
+        [DisplayName("Отправка")]
+        public bool IsSent
+        {
+            get { return _isSent; }
+            set
+            {
+                if (_isSent != value)
+                {
+                    _isSent = value;
+                    _OnPropertyChanged(this, "IsSent");
+                }
+            }
+        }
+
+        public MonitorCrew()
+        {
+
+        }
+
+        public MonitorCrew(kom kom)
+        {
+            //_departureDate = (kom.D_OTPR ?? DateTime.MinValue).ToString("dd.MM");
+            //_crewNumber = kom.N_KOM;
+            //_forcesType = kom.V_VS;
+            //_district = kom.V_OKRUG;
+            //_rrStation = kom.ST;
+            //_unit = kom.V_CH;
+            //_duty = kom.NARAD == 0 ? string.Empty : kom.NARAD.ToString();
+            //_overall = kom.VSEGO == 0 ? string.Empty : kom.VSEGO.ToString();
+            //_va = kom.VA == 0 ? string.Empty : kom.VA.ToString();
+            //_mtlb = kom.MTLB == 0 ? string.Empty : kom.MTLB.ToString();
+            //_access = kom.DOPUSK == 0 ? string.Empty : kom.DOPUSK.ToString();
+            //_elder = !string.IsNullOrEmpty(kom.PREDS) ? ElderArrived : string.Empty;
+
+            //_dressUp = kom.FL_PEREOD != null && kom.FL_PEREOD == 1;
+            //_isSent = !string.IsNullOrEmpty(kom.NVESHAT) || (kom.KOLSUHPAY != null && kom.KOLSUHPAY > 0);
+            UpdateWithKom(kom);
+        }
+
+        private void _OnPropertyChanged(object sender, string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void UpdateWithKom(kom kom)
+        {
+            DepartureDate = (kom.D_OTPR ?? DateTime.MinValue).ToString("dd.MM");
+            CrewNumber = kom.N_KOM;
+            ForcesType = kom.V_VS;
+            District = kom.V_OKRUG;
+            RRStation = kom.ST;
+            Unit = kom.V_CH;
+            Duty = kom.NARAD == 0 ? string.Empty : kom.NARAD.ToString();
+            Overall = kom.VSEGO == 0 ? string.Empty : kom.VSEGO.ToString();
+            VA = kom.VA == 0 ? string.Empty : kom.VA.ToString();
+            MTLB = kom.MTLB == 0 ? string.Empty : kom.MTLB.ToString();
+            Access = kom.DOPUSK == 0 ? string.Empty : kom.DOPUSK.ToString();
+            Elder = !string.IsNullOrEmpty(kom.PREDS) ? ElderArrived : string.Empty;
+
+            DressUp = kom.FL_PEREOD != null && kom.FL_PEREOD == 1;
+            IsSent = !string.IsNullOrEmpty(kom.NVESHAT);
+            //IsSent = !string.IsNullOrEmpty(kom.NVESHAT) || (kom.KOLSUHPAY != null && kom.KOLSUHPAY > 0);
+        }
+    }
+}
